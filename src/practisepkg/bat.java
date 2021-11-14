76247f681f0ffc20cb36ca37590863a365c9b75a
santos naidu <santhosh333nd@gmail.com>
2021-11-14 19:44:01 +0530

    adding the second task

--- src/practisepkg/bat.java
+++ src/practisepkg/bat.java
@@ -8,6 +8,7 @@ public class bat extends apple{
 	
 	void display() {
 		
+		//here im writing 202 task- just ignore and this is for eg for gitstashupdate
 		System.out.println(super.bikespeed);
 		System.out.println(super.suzuki);
 		
